efdb4d30ed16dbde74e10f4f6f1c9993498f49bb
Trang Vy <mvy@hmc.edu>
2015-09-27 01:35:37 -0700

    testing out ceter cut

--- image_alignment/Russian_Image_Alignment.m
+++ image_alignment/Russian_Image_Alignment.m
@@ -10,10 +10,10 @@ close all
 % The next few lines define variables for the locations and types of image files
 % we will be reading and writing. You  will likely want to change the input and
 % output directories to match you personal environment.
-input_dir = '/common/cs/cs181m/data/prokudin-gorsky/';
-output_dir = '/home/tbarton/Documents/MATLAB/';
-file_ext = 'tif';
-file_name = '01648a.tif';
+input_dir = '';
+output_dir = '';
+file_ext = 'png';
+file_name = 'example.png';
 
 
 
@@ -33,25 +33,39 @@ B = I(1:height,:);
 G = I(height+1:2*height,:);
 R = I(2*height+1:3*height,:);
 
+% [B_height, B_width] = size(B);
+% [G_height, G_width] = size(G);
+% [R_height, R_width] = size(R);
+% 
+% Bcropped = imcrop(B, [B_width*0.2 B_height*0.2 B_width*0.8 B_height*0.8]);
+% Gcropped = imcrop(G, [G_width*0.2 G_height*0.2 G_width*0.8 G_height*0.8]);
+% Rcropped = imcrop(R, [R_width*0.2 R_height*0.2 R_width*0.8 R_height*0.8]);
 
-
-%% use pyramid scheme to find optimal fit
-[R_x_shift, R_y_shift] = pyramid_scheme_tnt(B, R);
-[G_x_shift, G_y_shift] = pyramid_scheme_tnt(B, G);
-
-
-%% Display color channels
-% Now we display each of the color channels separately in a new window.
-newG = circshift(G, [G_x_shift, G_y_shift] );
-newR = circshift(R, [R_x_shift, R_y_shift] );
-output = cat(3,newR, newG, B);
-
- figure;
- subplot(1, 2, 1);
- imshow(cat(3, R, G, B));
- subplot(1, 2, 2);
- imshow(output)
- 
- 
- 
-
+figure;
+subplot(1, 3, 1);
+imshow(I);
+subplot(1, 3, 2);
+imshow(G);
+subplot(1, 3, 3);
+imshow(R);
+% 
+% %% use pyramid scheme to find optimal fit
+% [R_x_shift, R_y_shift] = pyramid_scheme_tnt(Bcropped, Rcropped);
+% [G_x_shift, G_y_shift] = pyramid_scheme_tnt(Bcropped, Gcropped);
+% 
+% 
+% %% Display color channels
+% % Now we display each of the color channels separately in a new window.
+% newG = circshift(G, [G_x_shift, G_y_shift] );
+% newR = circshift(R, [R_x_shift, R_y_shift] );
+% output = cat(3,newR, newG, B);
+% 
+%  figure;
+%  subplot(1, 2, 1);
+%  imshow(cat(3, R, G, B));
+%  subplot(1, 2, 2);
+%  imshow(output)
+%  
+%  
+%  
+% 
