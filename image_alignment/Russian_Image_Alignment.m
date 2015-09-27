77a4c8afd8ea2d49c6d3e7d4107740943cd77e28
Trang Vy <mvy@hmc.edu>
2015-09-27 01:37:24 -0700

    completed center cut pre-processing step

--- image_alignment/Russian_Image_Alignment.m
+++ image_alignment/Russian_Image_Alignment.m
@@ -2,20 +2,15 @@
 %% Clean up
 % These functions calls clean up the MATLAB environment and close all windows
 % open "extra" windows.
-clear all
-close all
-
 
 %% Variables
 % The next few lines define variables for the locations and types of image files
 % we will be reading and writing. You  will likely want to change the input and
 % output directories to match you personal environment.
-input_dir = '';
-output_dir = '';
-file_ext = 'png';
-file_name = 'example.png';
-
-
+input_dir = '\\HMC.Edu\HMCDFS\HMC_2016\mvy\My Documents\prokudin-gorsky\';
+output_dir = '/home/tbarton/Documents/MATLAB/';
+file_ext = 'tif';
+file_name = '00899a.tif';
 
 %% Read image file
 % Here we read the input jpg file into a 3D array of 8-bit integers. Before we
@@ -33,39 +28,27 @@ B = I(1:height,:);
 G = I(height+1:2*height,:);
 R = I(2*height+1:3*height,:);
 
-% [B_height, B_width] = size(B);
-% [G_height, G_width] = size(G);
-% [R_height, R_width] = size(R);
+[Bheight, Bwidth] = size(B);
+[Gheight, Gwidth] = size(G);
+[Rheight, Rwidth] = size(R);
 % 
-% Bcropped = imcrop(B, [B_width*0.2 B_height*0.2 B_width*0.8 B_height*0.8]);
-% Gcropped = imcrop(G, [G_width*0.2 G_height*0.2 G_width*0.8 G_height*0.8]);
-% Rcropped = imcrop(R, [R_width*0.2 R_height*0.2 R_width*0.8 R_height*0.8]);
+Bcropped = imcrop(B, [Bwidth*0.2 Bheight*0.2 Bwidth*0.6 Bheight*0.6]);
+Gcropped = imcrop(G, [Gwidth*0.2 Gheight*0.2 Gwidth*0.6 Gheight*0.6]);
+Rcropped = imcrop(R, [Rwidth*0.2 Rheight*0.2 Rwidth*0.6 Rheight*0.6]);
 
-figure;
-subplot(1, 3, 1);
-imshow(I);
-subplot(1, 3, 2);
-imshow(G);
-subplot(1, 3, 3);
-imshow(R);
-% 
-% %% use pyramid scheme to find optimal fit
-% [R_x_shift, R_y_shift] = pyramid_scheme_tnt(Bcropped, Rcropped);
-% [G_x_shift, G_y_shift] = pyramid_scheme_tnt(Bcropped, Gcropped);
-% 
-% 
-% %% Display color channels
-% % Now we display each of the color channels separately in a new window.
-% newG = circshift(G, [G_x_shift, G_y_shift] );
-% newR = circshift(R, [R_x_shift, R_y_shift] );
-% output = cat(3,newR, newG, B);
-% 
-%  figure;
-%  subplot(1, 2, 1);
-%  imshow(cat(3, R, G, B));
-%  subplot(1, 2, 2);
-%  imshow(output)
-%  
-%  
-%  
-% 
+%% use pyramid scheme to find optimal fit
+[R_x_shift, R_y_shift] = pyramid_scheme_tnt(Bcropped, Rcropped);
+[G_x_shift, G_y_shift] = pyramid_scheme_tnt(Bcropped, Gcropped);
+
+
+%% Display color channels
+% Now we display each of the color channels separately in a new window.
+newG = circshift(G, [G_x_shift, G_y_shift] );
+newR = circshift(R, [R_x_shift, R_y_shift] );
+output = cat(3,newR, newG, B);
+
+ figure;
+ subplot(1, 2, 1);
+ imshow(cat(3, R, G, B));
+ subplot(1, 2, 2);
+ imshow(output)
