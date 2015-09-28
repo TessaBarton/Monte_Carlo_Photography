8d5b82994eae3e64779c179cb2933ca95f009bbb
Trang Vy <mvy@hmc.edu>
2015-09-27 23:01:55 -0700

    reverted shift_image_tnt to not have parallelism

--- image_alignment/shift_image_tnt.m
+++ image_alignment/shift_image_tnt.m
@@ -1,10 +1,8 @@
 function [x_shift, y_shift] = shift_image_tnt( target, image, window, x_guess, y_guess)
  
-SSDmap = containers.Map();
+lowestSSD = Inf;  
 
-% v_offsets = x_guess - window : x_guess + window;
-% h_offsets = y_guess - window : y_guess + window;
-parfor v_offset = x_guess - window : x_guess + window
+for v_offset = x_guess - window : x_guess + window
     for h_offset = y_guess - window : y_guess + window
         
         shiftedImage = circshift(image, [v_offset h_offset]);
@@ -12,13 +10,10 @@ parfor v_offset = x_guess - window : x_guess + window
         %% Loop through entire image and calculate SSD
         SSD = sum(sum((target-shiftedImage).^2));
         %% Replace the SSD if this offset produces better match
-%         if(SSD < lowestSSD)
-%             lowestSSD = SSD;
-%             x_shift = v_offset;
-%             y_shift = h_offset;
-%         end
-        SSDmap(int2str(SSD)) = [v_offset, h_offset];
+        if(SSD < lowestSSD)
+            lowestSSD = SSD;
+            x_shift = v_offset;
+            y_shift = h_offset;
+        end
     end
 end
-
-SSDs = keys(SSDmap);
